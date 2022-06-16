1e4b5b86f9489b21fbd6f13f2fca50a0568bfae8
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-06-16 11:31:43 +0200

    feat(featured) : ajout de Featured

--- src/featured/Featured.js
+++ src/featured/Featured.js
@@ -1,8 +1,17 @@
+import { MoreVert } from '@mui/icons-material';
 import React from 'react';
 import './featured.scss';
 
 const Featured = () => {
-	return <div className="featured">Featured</div>;
+	return (
+		<div className="featured">
+			<div className="top">
+				<h1 className="title">Total Revenue</h1>
+				<MoreVert fontSize="small" />
+			</div>
+			<div className="bottom"></div>
+		</div>
+	);
 };
 
 export default Featured;
