f8f3c6ec125c331aac9c25c596d2b8d7458f4353
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-06-20 14:27:22 +0200

    feat(component) : ajout de Single page

--- src/pages/single/Single.js
+++ src/pages/single/Single.js
@@ -3,6 +3,7 @@ import './single.scss';
 import Sidebar from '../../components/sidebar/Sidebar';
 import Navbar from '../../components/navbar/Navbar';
 import Chart from '../../components/chart/Chart';
+import List from '../../components/table/Table.js';
 
 const Single = () => {
 	return (
@@ -48,7 +49,10 @@ const Single = () => {
 						/>
 					</div>
 				</div>
-				<div className="bottom"></div>
+				<div className="bottom">
+					<h1 className="title">Dernières transactions</h1>
+					<List />
+				</div>
 			</div>
 		</div>
 	);
