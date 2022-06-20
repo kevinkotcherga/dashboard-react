28986b6f35214267dda022d421a6235fc93ec4ca
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-06-20 16:34:51 +0200

    feat(component) : ajout de la fonction supprimer dans Datatable

--- src/components/datatable/Datatable.js
+++ src/components/datatable/Datatable.js
@@ -1,22 +1,32 @@
 import { DataGrid } from '@mui/x-data-grid';
-import React from 'react';
+import React, { useState } from 'react';
 import './datatable.scss';
 import { userColumns, userRows } from '../../datatablesource';
 import { Link } from 'react-router-dom';
 
 const Datatable = () => {
+	const [data, setData] = useState(userRows);
+
+	const handleDelete = id => {
+		setData(data.filter(item => item.id !== id));
+	};
 	const actionColumn = [
 		{
 			field: 'action',
 			headerName: 'Action',
 			width: 200,
-			renderCell: () => {
+			renderCell: params => {
 				return (
 					<div className="cellAction">
 						<Link to="/users/test" style={{ textDecoration: 'none' }}>
-							<div className="viewButton">Voir</div>
+							<div className="viewButton">View</div>
 						</Link>
-						<div className="deleteButton">Supprimer</div>
+						<div
+							className="deleteButton"
+							onClick={() => handleDelete(params.row.id)}
+						>
+							Delete
+						</div>
 					</div>
 				);
 			},
