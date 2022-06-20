1040b09ba1baa96e4f19694ce55f5dccab605385
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-06-20 15:16:31 +0200

    feat(page) : ajout de la page New

---
+++ src/formSource.js
@@ -0,0 +1,76 @@
+export const userInputs = [
+	{
+		id: 1,
+		label: 'Pseudo',
+		type: 'text',
+		placeholder: 'john_doe',
+	},
+	{
+		id: 2,
+		label: 'Nom et surnom',
+		type: 'text',
+		placeholder: 'John Doe',
+	},
+	{
+		id: 3,
+		label: 'Email',
+		type: 'mail',
+		placeholder: 'john_doe@gmail.com',
+	},
+	{
+		id: 4,
+		label: 'Téléphone',
+		type: 'text',
+		placeholder: '0606060606',
+	},
+	{
+		id: 5,
+		label: 'Mot de passe',
+		type: 'password',
+	},
+	{
+		id: 6,
+		label: 'Adresse',
+		type: 'text',
+		placeholder: 'Elton St. 216 NewYork',
+	},
+	{
+		id: 7,
+		label: 'Pays',
+		type: 'text',
+		placeholder: 'USA',
+	},
+];
+
+export const productInputs = [
+	{
+		id: 1,
+		label: 'Titre',
+		type: 'text',
+		placeholder: 'Apple Macbook Pro',
+	},
+	{
+		id: 2,
+		label: 'Description',
+		type: 'text',
+		placeholder: 'Description',
+	},
+	{
+		id: 3,
+		label: 'Categories',
+		type: 'text',
+		placeholder: 'Computers',
+	},
+	{
+		id: 4,
+		label: 'Prix',
+		type: 'text',
+		placeholder: '100',
+	},
+	{
+		id: 5,
+		label: 'Stock',
+		type: 'text',
+		placeholder: 'in stock',
+	},
+];
