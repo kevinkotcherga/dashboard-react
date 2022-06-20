7e7ac58d25568f36f30356b3320c7ee8280a4bde
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-06-20 16:29:51 +0200

    feat(context) : ajout du fichier context et du darkMode avec useContext

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './sidebar.scss';
 import DashboardIcon from '@mui/icons-material/Dashboard';
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
@@ -13,8 +13,11 @@ import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
 import AccountCircleOutlinedIcon from '@mui/icons-material/AccountCircleOutlined';
 import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined';
 import { Link } from 'react-router-dom';
+import { DarkModeContext } from '../../context/darkModeContext';
 
 const Sidebar = () => {
+	const { dispatch } = useContext(DarkModeContext);
+
 	return (
 		<div className="sidebar">
 			<div className="top">
@@ -85,8 +88,14 @@ const Sidebar = () => {
 				</ul>
 			</div>
 			<div className="bottom">
-				<div className="colorOption"></div>
-				<div className="colorOption"></div>
+				<div
+					className="colorOption"
+					onClick={() => dispatch({ type: 'LIGHT' })}
+				></div>
+				<div
+					className="colorOption"
+					onClick={() => dispatch({ type: 'DARK' })}
+				></div>
 			</div>
 		</div>
 	);
